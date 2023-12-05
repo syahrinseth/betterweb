150f531a86a5698ae822a6b8d174e0f3e2a80b8e
syahrinseth <syahrinsethservices@gmail.com>
2023-12-05 22:47:48 +0300

    ziggy https server fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,9 @@ import '../css/app.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { Ziggy } from './ziggy.js';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+// import { ZiggyVue } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
