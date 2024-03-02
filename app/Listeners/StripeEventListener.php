31d996b67dfe81e40804d6f23d1ed927dce2bea8
syahrinseth <syahrinsethservices@gmail.com>
2024-03-02 15:42:19 +0300

    added: stripe webhooks

---
+++ app/Listeners/StripeEventListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use Laravel\Cashier\Events\WebhookReceived;
+
+class StripeEventListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(WebhookReceived $event): void
+    {
+        if ($event->payload['type'] === 'invoice.payment_succeeded') {
+            // Handle the incoming event...
+        }
+    }
+}
