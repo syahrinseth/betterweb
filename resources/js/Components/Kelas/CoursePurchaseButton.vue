551e52c0f2a926d88174431bc5decc9c2eef60d2
syahrinseth <syahrinsethservices@gmail.com>
2024-03-03 17:30:53 +0300

    fix: text in purchase button

--- resources/js/Components/Kelas/CoursePurchaseButton.vue
+++ resources/js/Components/Kelas/CoursePurchaseButton.vue
@@ -11,7 +11,8 @@ const props = defineProps({
         <Link v-else :href="route('kelas.register', { callback_url: $page.url })" class="text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:ring-cyan-300 font-extrabold rounded-lg text-xs sm:text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-cyan-600 dark:hover:bg-cyan-700 focus:outline-none dark:focus:ring-cyan-800 bg-gradient-to-r from-red-700 to-blue-500 transition duration-300 ease-in-out hover:scale-105"> 
             Mula Belajar Dengan <span class="line-through text-gray-300 font-light">RM199</span> RM99
         </Link>
-        <p class="mt-2 text-xs dark:text-gray-300">*Dengan Kupon Diskaun (terhad kepada 100 orang pertama sahaja)</p>
+        <p class="mt-2 text-xs dark:text-gray-300">*Dengan Kupon Diskaun </p>
+        <p class="text-xs dark:text-gray-300">(terhad kepada 100 orang pertama sahaja)</p>
     </div>
     
 </template>
