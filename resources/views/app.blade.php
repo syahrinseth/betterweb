c60d9e4e1be8bce9aa11d0fc33b52a941726fc83
syahrinseth <syahrinsethservices@gmail.com>
2024-04-16 12:03:57 +0300

    Added Video Length for Course lessons & Apply Tawk and Firebase analytic for prod environment

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,41 +21,43 @@
         
         @inertia
 
-        <!--Start of Tawk.to Script-->
-        <script type="text/javascript">
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/656f1fa3ff45ca7d478707ec/1hgt1763b';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-        </script>
-        <!--End of Tawk.to Script-->
-        <script type="module">
-            // Import the functions you need from the SDKs you need
-            import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-            import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
-            // TODO: Add SDKs for Firebase products that you want to use
-            // https://firebase.google.com/docs/web/setup#available-libraries
+        @if (env('APP_ENV') == 'prod' || env('APP_ENV') == 'production')
+            <!--Start of Tawk.to Script-->
+            <script type="text/javascript">
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/656f1fa3ff45ca7d478707ec/1hgt1763b';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+            </script>
+            <!--End of Tawk.to Script-->
+            <script type="module">
+                // Import the functions you need from the SDKs you need
+                import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+                import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+                // TODO: Add SDKs for Firebase products that you want to use
+                // https://firebase.google.com/docs/web/setup#available-libraries
 
-            // Your web app's Firebase configuration
-            // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-            const firebaseConfig = {
-                apiKey: "AIzaSyDI6o1eGUQoP8q0Ab3TTaG9WUTiAUSki0s",
-                authDomain: "betterweb-99b09.firebaseapp.com",
-                projectId: "betterweb-99b09",
-                storageBucket: "betterweb-99b09.appspot.com",
-                messagingSenderId: "66171088389",
-                appId: "1:66171088389:web:72d320fa9484be75c4f66c",
-                measurementId: "G-4P171N4BN5"
-            };
+                // Your web app's Firebase configuration
+                // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+                const firebaseConfig = {
+                    apiKey: "AIzaSyDI6o1eGUQoP8q0Ab3TTaG9WUTiAUSki0s",
+                    authDomain: "betterweb-99b09.firebaseapp.com",
+                    projectId: "betterweb-99b09",
+                    storageBucket: "betterweb-99b09.appspot.com",
+                    messagingSenderId: "66171088389",
+                    appId: "1:66171088389:web:72d320fa9484be75c4f66c",
+                    measurementId: "G-4P171N4BN5"
+                };
 
-            // Initialize Firebase
-            const app = initializeApp(firebaseConfig);
-            const analytics = getAnalytics(app);
-        </script>
+                // Initialize Firebase
+                const app = initializeApp(firebaseConfig);
+                const analytics = getAnalytics(app);
+            </script>
+        @endif
     </body>
 </html>
